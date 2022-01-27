a4479ab86de3004f9ae83520e23256822a53c820
Fei Wu <feiwu00@outlook.com>
2022-01-27 07:46:16 -0600

    Add files via upload

--- Installation/R_packages_installation.R
+++ Installation/R_packages_installation.R
@@ -5,14 +5,10 @@ BiocManager::install(version = "3.14")
 BiocManager::install(c("biomaRt","DESeq2","tximeta","limma","phyloseq","glmGamPoi","cmapR","MAST",
                         "microbiome","ANCOMBC","Maaslin2","DO.db","clusterProfiler","enrichplot","pathview"))
 
-if (!require("nloptr")) install.packages("nloptr",repos = "http://cran.us.r-project.org")
-
 if (!require("pacman")) install.packages("pacman",repos = "http://cran.us.r-project.org")
 pacman::p_load(tidyverse,ggrepel,colorspace,RColorBrewer,
                 pheatmap,VennDiagram,doParallel,foreach,
                 stringi,vegan,ggpubr,reshape2,sctransform,hdf5r,
                 ggridges,ggnewscale,ggupset,Seurat)
-
-if (!require("Seurat")) install.packages("Seurat",repos = "http://cran.us.r-project.org")
 
 #included in tidyverse: ggplot2,tidyr,dplyr,stringr,tibble,
